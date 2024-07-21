6ec2ade08bdcafc7e0b975dbe888ddf19ae3adb8
Deepak Singh <97837264+DeepakS-02@users.noreply.github.com>
2024-07-21 11:07:51 +0530

    Delete Frontend-Final-main directory

--- Frontend-Final-main/src/routes.js
+++
@@ -1,8 +0,0 @@
-import ProductDetails from "./components/ProductDetails";
-import ProductList from "./components/ProductList";
-
-
-export const routes=[
-    {path:"/" ,element:<ProductList/>},
-    {path:"/product/:pid" ,element:<ProductDetails/>}
-]
